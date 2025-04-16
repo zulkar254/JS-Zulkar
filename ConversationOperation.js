00908639d9a2be7ce6e40d31753bbc74049e435e
zulkar254 <zulkardbg1458@gmail.com>
2025-04-16 19:36:07 +0530

    ConversationOperation

--- ConversationOperation.js
+++ ConversationOperation.js
@@ -21,6 +21,52 @@
  // 1=> true ; 0=>false
  //""=> false; "zulkar"=>true
 
- let someNumber=33 
- let StringNumber=String(someNumber)
- console.log( typeof StringNumber);
+// let someNumber=33 
+ //let StringNumber=String(someNumber)
+ //console.log( typeof StringNumber);
+
+
+ //********************************************************Operations*************************************************************** */
+//  let value=3
+//  let negValue=-value
+ //console.log(negValue);
+
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2**3);
+//  console.log(2/3);
+//  console.log(2%3);
+
+//  let str1 ="hello "
+//  let str2 ="   zulkar"
+
+//  let str3= str1+str2
+
+//  console.log(str3);
+
+//  console.log("1" +2);
+//  console.log(1 +"2" +2 );
+//  console.log(1 +2 +"2" );
+
+//  console.log( (3+2) *5 %2);
+
+//  console.log (+true);
+//  //console.log (true+);
+
+//  let num1, num2,num3
+
+//  num1=num2=num3=4+4+4
+
+//  console.log(num2);
+
+//  let gameCounter=100
+//  gameCounter++;
+//  console.log(gameCounter);
+
+ let gameCounter=100
+ ++gameCounter;
+ console.log(gameCounter);
+ 
+ 
+
