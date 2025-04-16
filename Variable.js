7b62d2fa5aa167f6dcebd29a3ea04a484d76c09e
Zulkar <abdullah262001@gmail.com>
2025-04-16 16:42:58 +0530

    Initial commit

---
+++ Variable.js
@@ -0,0 +1,12 @@
+const accountId=8857674
+let accountName="zulkar"
+var accountEmail="zulkardbg1458@gmail.com"
+accountCity="Bihar"
+
+//accountId=2// not allowed
+accountEmail="jeiwjigrj"
+accountCity="Delhi"
+accountName="CHIRAG"
+
+//console.log(accountId);
+console.table([accountName,accountEmail,accountCity]);
