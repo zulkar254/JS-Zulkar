1fa8b997333fd70eb31797af9b99d0d50f1e99d5
zulkar254 <zulkardbg1458@gmail.com>
2025-04-16 18:34:42 +0530

    datatypes

---
+++ DataType.js
@@ -0,0 +1,18 @@
+//let isLoggedIn="false"
+//let state;
+let  age =27 ;
+
+//number =>2 to power 53
+//bigint 
+//string=>""
+// boolean=>true/false
+//null=> standlone
+//undefined =>
+    //sysmbol=>
+
+
+
+//object
+
+
+console.log(typeof age);
--- README.md
+++
@@ -1,2 +0,0 @@
-# JS-Zulkar
-A code repo 
