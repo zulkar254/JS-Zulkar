191bf6d9f27382ae6b43e0dbf2f94c00a4085202
zulkar254 <zulkardbg1458@gmail.com>
2025-04-16 18:55:12 +0530

    ConversationOperation

---
+++ ConversationOperation.js
@@ -0,0 +1,26 @@
+//let scroe = "zulkar"
+
+//console.log(typeof scroe);
+//console.log(typeof(scroe));
+
+//let valueInNumber =Number(scroe)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber );
+
+//if a number 33 then easily convert in to numer
+//if a number is 33abc then  convert into NAN
+//if number is true  then convert 1 and same false convert into 0
+
+//let isLoggedIn="zulkar"
+
+ //let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+ //console.log(booleanIsLoggedIn);
+
+
+ // 1=> true ; 0=>false
+ //""=> false; "zulkar"=>true
+
+ let someNumber=33 
+ let StringNumber=String(someNumber)
+ console.log( typeof StringNumber);
